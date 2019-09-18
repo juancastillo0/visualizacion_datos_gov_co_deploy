d08c7458bfce0751fee53b7c99ed8c82b4d577b0
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2019-09-17 20:32:48 -0500

    first

---
+++ precache-manifest.14bc4636b22b7b401bdd0a3d3cfe929d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4fc6950505952aff746cad0e4942d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "931a3364b152cba8ea98",
+    "url": "/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "fabe5396018f95fb99a5",
+    "url": "/static/js/2.3c1eae92.chunk.js"
+  },
+  {
+    "revision": "931a3364b152cba8ea98",
+    "url": "/static/js/main.d2be7b79.chunk.js"
+  },
+  {
+    "revision": "a1dc2bad9d8d64af8893",
+    "url": "/static/js/runtime~main.997d93f9.js"
+  }
+]);
